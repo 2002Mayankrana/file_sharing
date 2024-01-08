431ed53b3b1281d727067ace1dced49d4b242936
Mayank Rana <118000661+2002Mayankrana@users.noreply.github.com>
2024-01-08 11:41:20 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8000';
+const API_URI = 'https://backend-fvsb.onrender.com/api/v1';
 
 export const uploadFile = async (data) => {
     try {
